51a221eee0f555f9127d53bc35a032cbc8069f68
Aariya <ST10029788@vcconnect.edu.za>
2024-05-04 20:56:25 +0200

    Create MVC File

---
+++ Agri_Energy_Connect_PROG7311_POE/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Agri_Energy_Connect_PROG7311_POE.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
