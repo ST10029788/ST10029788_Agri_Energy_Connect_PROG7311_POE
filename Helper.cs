8b27220a2edfd88b8cefefc335298286bdfef71a
ST10029788 <103359783+ST10029788@users.noreply.github.com>
2024-05-24 13:17:40 +0200

    Add files via upload

---
+++ Helper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AgriEnergyConnectApp
+{
+    public class Helper
+    {
+        public static String Employee_ID = "EMP001";
+        public static String frmID;
+    }
+}
